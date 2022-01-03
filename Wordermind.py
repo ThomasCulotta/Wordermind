ce3033ac8b754af5a654dadde129c4c8255e100d
Thomas <thomasculotta94@gmail.com>
2022-01-02 16:05:42 -0800

    only allow words in word list; improve letter search results

--- Wordermind.py
+++ Wordermind.py
@@ -2,7 +2,7 @@ import random, re
 
 filename = "dict.txt"
 with open(filename, "r") as file:
-    words = file.readlines()
+    words = [word.strip() for word in file.readlines()]
 
 prntReset = "\033[0m"
 prntBold = "\033[01m"
@@ -12,45 +12,64 @@ prntGrey = "\033[90m"
 prntBlock = "█"
 
 maxChances = 6
-chancesTaken = 6
-keyWord = random.choice(words).strip()
+chancesTaken = 7
+keyWord = random.choice(words)
 resultBoard = []
 
+print(keyWord)
 print("Guess the 5 letter word. 6 guesses.\n")
-
-for chance in range(1, maxChances + 1):
+for chance in range(maxChances):
     guessWord = input().lower()
+    resultBoard.append([prntBlock for index in range(5)])
 
-    while re.match("[a-z]{5}", guessWord) == None:
-        print("Invalid word. Try again: ", end="")
+    while guessWord not in words:
+        print("Not in word list. Try again: ", end="")
         guessWord = input().lower()
 
-    result = ""
-    for index in range(5):
-        if guessWord[index] == keyWord[index]:
-            print(prntBold + prntGreen + guessWord[index].upper() + prntReset, end="")
-            result += prntGreen + prntBlock + prntReset
-        elif guessWord[index] in keyWord:
-            print(prntBold + prntYellow + guessWord[index].upper() + prntReset, end="")
-            result += prntYellow + prntBlock + prntReset
-        else:
-            print(prntGrey + guessWord[index] + prntReset, end="")
-            result += prntGrey + prntBlock + prntReset
+    guessingWord = list(guessWord)
+    searchingWord = list(keyWord)
+    printWord = ["." for index in range(5)]
 
-    resultBoard.append(result)
-    print()
+    # Find correct letters
+    for index in range(5):
+        if guessingWord[index] == searchingWord[index]:
+            printWord[index] = prntBold + prntGreen + guessingWord[index].upper() + prntReset
+            resultBoard[chance][index] = prntGreen + prntBlock + prntReset
+            searchingWord[index] = "."
+            guessingWord[index] = "."
+
+    # Find out of place letters
+    for index in range(5):
+        if guessingWord[index] == ".":
+            continue
+
+        if guessingWord[index] in searchingWord:
+            if guessingWord[index + 1:].count(guessingWord[index]) < searchingWord.count(guessingWord[index]):
+                printWord[index] = prntBold + prntYellow + guessingWord[index].upper() + prntReset
+                resultBoard[chance][index] = prntYellow + prntBlock + prntReset
+                guessingWord[index] = "."
+
+    # Wrong letters
+    for index in range(5):
+        if guessingWord[index] == ".":
+            continue
+
+        printWord[index] = prntGrey + guessingWord[index] + prntReset
+        resultBoard[chance][index] = prntGrey + prntBlock + prntReset
+
+    print("".join(printWord) + f" {chance + 1}/{maxChances}")
 
     if guessWord == keyWord:
-        chancesTaken = chance
+        chancesTaken = chance + 1
         break
 
 print()
 
-if chancesTaken == 6:
+if chancesTaken == 7:
     print(prntBold + keyWord + prntReset)
     print()
-
-print(f"{chancesTaken}/{maxChances}")
+else:
+    print(f"{chancesTaken}/{maxChances}")
 
 for result in resultBoard:
-    print(result)
+    print("".join(result))
